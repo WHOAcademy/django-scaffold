51187ca2b38cac9380006e9909212ab473299148
Aykut Bulgu <mabulgu@gmail.com>
2020-07-16 20:28:43 +0300

    fix naming problems

--- django_app/views.py
+++ django_app/views.py
@@ -10,12 +10,12 @@ from .serializers import PostSerializer
 
 def post_list(request):
     posts = Post.objects.order_by('-published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'django_app/post_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'django_app/post_detail.html', {'post': post})
 
 
 def post_new(request):
@@ -29,7 +29,7 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'django_app/post_edit.html', {'form': form})
 
 
 def post_edit(request, pk):
@@ -44,7 +44,7 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'django_app/post_edit.html', {'form': form})
 
 
 class PostViewSet(viewsets.ModelViewSet):
