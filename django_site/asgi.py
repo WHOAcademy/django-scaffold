7048b667aa60de2326699408b72fcfb939e128a9
Aykut Bulgu <mabulgu@gmail.com>
2020-07-17 03:20:03 +0300

    remove load data

--- django_site/asgi.py
+++ django_site/asgi.py
@@ -10,9 +10,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from django_site.preps import set_default_env, load_sample_data
+from django_site.preps import set_default_env
 
 set_default_env()
-load_sample_data()
 
 application = get_asgi_application()
--- django_site/wsgi.py
+++ django_site/wsgi.py
@@ -10,9 +10,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django_site.preps import set_default_env, load_sample_data
+from django_site.preps import set_default_env
 
 set_default_env()
-load_sample_data()
 
 application = get_wsgi_application()
